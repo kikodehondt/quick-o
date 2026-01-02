9a0a2fd469790dbfa15927d5a1a1c31f6fea6040
kikodehondt <kikodehondt06@gmail.com>
2026-01-02 09:49:56 +0100

    Versie 1 / Bèta - " speed insights\

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './lib/authContext.tsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
       <App />
+      <SpeedInsights />
     </AuthProvider>
   </StrictMode>,
 )
